526fd4ba4ab4d1a1d2c23f576bea0fce72300fb8
Srimped <nickey200202@gmail.com>
2024-03-26 04:03:56 +0700

    account model

--- NanaProject/Models/Account.cs
+++ NanaProject/Models/Account.cs
@@ -3,5 +3,9 @@ using System.ComponentModel.DataAnnotations;
 namespace NanaProject.Models;
 public class Account
 {
-    
+    public int AccId { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Phone { get; set; }
+    public string Address { get; set; }
 }
