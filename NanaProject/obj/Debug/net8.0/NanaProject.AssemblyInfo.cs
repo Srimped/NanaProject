d26769d5a136f2b678d802e3cb6df065fab08443
Srimped <nickey200202@gmail.com>
2024-03-26 22:53:18 +0700

    Account Controller

--- NanaProject/obj/Debug/net8.0/NanaProject.AssemblyInfo.cs
+++ NanaProject/obj/Debug/net8.0/NanaProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NanaProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bd12bc5bfe3d61feeaef00a0999810bc5d32e4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+773f5d7a1873ec02122d7739f52b82c192caf521")]
 [assembly: System.Reflection.AssemblyProductAttribute("NanaProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NanaProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
