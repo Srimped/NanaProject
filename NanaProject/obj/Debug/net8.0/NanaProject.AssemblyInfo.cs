7be34094c7b864115b22a17752486e53bbec9c2d
Srimped <nickey200202@gmail.com>
2024-04-01 14:30:41 +0700

    ignore bin & obj

--- NanaProject/obj/Debug/net8.0/NanaProject.AssemblyInfo.cs
+++ NanaProject/obj/Debug/net8.0/NanaProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NanaProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22e24ed36afb97b376201bdb6f26d07314f567bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7549f9225714c733fd47e241ef2819c0d6500a31")]
 [assembly: System.Reflection.AssemblyProductAttribute("NanaProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NanaProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
