db8608f2efeb9626fbf8589af4563cb9975a4376
Srimped <nickey200202@gmail.com>
2024-04-02 07:51:19 +0700

    pet CRUD front-end & function

--- NanaProject/obj/Debug/net8.0/NanaProject.AssemblyInfo.cs
+++ NanaProject/obj/Debug/net8.0/NanaProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NanaProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4eedcec6b341c0a85ee13594c76395bdafdb780")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+546db6982d06a15b4d0192dfcd85893a0fe25d55")]
 [assembly: System.Reflection.AssemblyProductAttribute("NanaProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NanaProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
