4d589191f07ed2e5ea428490b4c69899ddd6ce76
Srimped <nickey200202@gmail.com>
2024-04-02 14:27:24 +0700

    obj

--- NanaProject/obj/Debug/net8.0/NanaProject.AssemblyInfo.cs
+++ NanaProject/obj/Debug/net8.0/NanaProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NanaProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7be34094c7b864115b22a17752486e53bbec9c2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5e24bce1f56d2eb5590e786b355c1df55d3ebeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("NanaProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NanaProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
